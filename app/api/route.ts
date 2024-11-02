d348ac48a20a74ede5580c09998b82997aab4c60
Benny Rahmat <akunbeben@gmail.com>
2024-11-03 05:03:34 +0800

    back to puppeteer

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,36 +1,39 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import chromium from "chrome-aws-lambda"
-import playwright from "playwright-core"
+import { NextRequest } from "next/server"
+import puppeteer from "puppeteer"
+import * as install from "puppeteer/internal/node/install.js"
 
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const browser = await playwright.chromium.launch({
-    args: chromium.args,
-    executablePath:
-      process.env.NODE_ENV !== "development"
-        ? await chromium.executablePath
-        : "/usr/bin/google-chrome",
-    headless: process.env.NODE_ENV !== "development" ? chromium.headless : true,
+export const maxDuration = 60
+export const revalidate = 300
+
+export async function GET(request: NextRequest) {
+  await install.downloadBrowsers()
+
+  const browser = await puppeteer.launch({
+    args: [
+      "--use-gl=angle",
+      "--use-angle=swiftshader",
+      "--single-process",
+      "--no-sandbox"
+    ],
+    headless: true,
   })
 
-  const page = await browser.newPage({
-    viewport: {
-      width: 1200,
-      height: 720,
-    },
-  })
+  try {
+    const page = await browser.newPage()
+    const url = request.nextUrl.searchParams.get("path") as string
+    await page.goto(url)
 
-  const url = req.query.path as string
+    const data = await page.screenshot({
+      type: "png",
+    })
 
-  await page.goto(url)
-
-  const data = await page.screenshot({
-    type: "png",
-  })
-
-  await browser.close()
-
-  // Set the `s-maxage` property to cache at the CDN layer
-  res.setHeader("Cache-Control", "s-maxage=31536000, public")
-  res.setHeader("Content-Type", "image/png")
-  res.end(data)
+    return new Response(data, {
+      headers: {
+        "Cache-Control": "s-maxage=31536000, public",
+        "Content-Type": "image/png"
+      }
+    })
+  } finally {
+    await browser.close()
+  }
 }
